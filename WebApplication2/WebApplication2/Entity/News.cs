a15c8825f38f9997b96911cdd84e211ac87a217e
Alexis <ouksel.alexis@hotmail.fr>
2018-10-16 15:32:32 +0200

    test

---
+++ WebApplication2/WebApplication2/Entity/News.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Entity
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string NewsPaper { get; set; }
+    }
+}
