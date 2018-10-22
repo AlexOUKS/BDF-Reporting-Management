c62bc59ac92e20e69d2658915784110d94d229f2
Alexis Ouksel <ouksel.alexis@hotmail.fr>
2018-10-22 17:16:08 +0200

    bye bye heroku

--- toProject/settings.py
+++ toProject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
 from django.core.exceptions import ImproperlyConfigured
 
 os.environ["DATABASE_NAME"] = "bdf"
@@ -137,6 +136,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
