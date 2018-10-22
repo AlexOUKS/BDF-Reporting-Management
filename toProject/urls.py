028de20b4ef94660de4b2e9a09897c2572b4b1c3
Alexis Ouksel <ouksel.alexis@hotmail.fr>
2018-10-22 17:20:30 +0200

    test

--- toProject/urls.py
+++ toProject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+
 urlpatterns = [
     path('authBDF/', include('authBDF.urls')),
     url(r'^admin/', admin.site.urls),
