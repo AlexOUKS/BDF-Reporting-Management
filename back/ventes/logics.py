d604c63c96addd408c31b2e4d0301279f5311ae4
AurelienBroutin <aurelien.broutin@utbm.fr>
2018-11-12 00:51:04 +0100

    update load csv

--- back/ventes/logics.py
+++ back/ventes/logics.py
@@ -8,7 +8,23 @@ import hashlib, datetime, json, string, random
 
 def load(request):
     if (request.method == "POST"):
-        if(request.FILES.content_type == 'csv'):
+        file = request.FILES["file"]
+        file2 = file
+        fileLines=file2.readlines()
+        if(file.content_type == 'text/csv'):
+            firstLine = file.readline().decode('utf-8')
+            print(firstLine)
+            secondLine = file.readline().decode('utf-8')
+            thridLine = file.readline().decode('utf-8')
+            #return HttpResponse(file.readline())
+            line= ""
+            print(len(fileLines)-1)
+            for i in range(3,len(fileLines)-1): # i est un type bytes (tableau d'array), need to decode
+                line = file.readline().decode('utf-8') # i2 est un type str
+
+#traiter les lignes
+            #for chunk in file.chunks():
+            return HttpResponse(line)
 
 
 
