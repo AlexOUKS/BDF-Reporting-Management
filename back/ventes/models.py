6557755d926567b4d6d174bf401ab33de747c610
AurelienBroutin <aurelien.broutin@utbm.fr>
2018-11-11 18:24:44 +0100

    correction fields to field vente

--- back/ventes/models.py
+++ back/ventes/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 import datetime
 
+class CategorieProduit(models.Model):
+    id = models.AutoField(primary_key = True)
+    nom= models.CharField(max_length = 30)
+    alt = models.CharField(max_length=30)
+
+class Produit(models.Model):
+    id = models.AutoField(primary_key = True)
+    idCategorieProduit = models.ForeignKey(CategorieProduit, on_delete = models.CASCADE)
+    nom= models.CharField(max_length = 30)
+    alt = models.CharField(max_length=30)
+    prixAchat = models.IntegerField()
+    prixVente = models.IntegerField()
+    disponible = models.BooleanField()
+
+class LieuDeVie(models.Model):
+    id = models.AutoField(primary_key = True)
+    alt = models.CharField(max_length=30)
+    nom= models.CharField(max_length = 30)
+
+
+class MethodePaiement(models.Model):
+    id = models.AutoField(primary_key = True)
+    alt = models.CharField(max_length=30)
+    nom= models.CharField(max_length = 30)
+
 class Vente(models.Model):
     id = models.AutoField(primary_key = True)
     dateVente = models.DateTimeField()
@@ -11,30 +36,11 @@ class Vente(models.Model):
     selledBy = models.CharField(max_length = 30)
 
 
-class Produit(models.Model):
-    id = models.AutoField(primary_key = True)
-    idCategorieProduit = models.ForeignKey(CategorieProduit, on_delete = models.CASCADE)
-    nom= models.CharFields(max_length = 30)
-    alt = models.CharField(max_length=30)
-    prixAchat = models.IntegerField()
-    prixVente = models.IntegerFiled()
-    disponible = models.BooleanFields()
 
 
-class CategorieProduit(models.Model):
-    id = models.AutoField(primary_key = True)
-    nom= models.CharFields(max_length = 30)
-    alt = models.CharField(max_length=30)
 
 
-class LieuDeVie(models.Model):
-    id = models.AutoField(primary_key = True)
-    alt = models.CharField(max_length=30)
-    nom= models.CharFields(max_length = 30)
 
 
-class MethodePaiement(models.Model):
-    id = models.AutoField(primary_key = True)
-    alt = models.CharField(max_length=30)
-    nom= models.CharFields(max_length = 30)
+
 
